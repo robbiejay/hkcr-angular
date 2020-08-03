45b8e87cb135c500bd2c1c30765ee152a0dc0d78
sephiroth420 <52matt85@gmail.com>
2020-08-03 04:10:05 -0700

    footer and body fixes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerService } from '../_services/player.service';
 
 @Component({
   selector: 'app-footer',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   currentYear = new Date().getDate();
-  constructor() { }
+  constructor( private playerService: PlayerService ) { }
 
   ngOnInit() {
   }
